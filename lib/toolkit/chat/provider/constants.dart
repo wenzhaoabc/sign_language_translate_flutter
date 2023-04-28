6f5f54ffc03ae0f085f0a5d92aa66a8f8fb413ab
qbdl <1260017352@qq.com>
2023-04-28 19:11:08 +0800

    url修改

--- lib/toolkit/chat/provider/constants.dart
+++ lib/toolkit/chat/provider/constants.dart
@@ -1,4 +1,5 @@
-const baseUrl = 'https://api.openai.com/v1';
+const baseUrl = 'http://43.153.22.13:8080/';
+// const baseUrl = 'https://api.openai.com/v1';
 const aiApiKey = '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';
 const aiModel = "gpt-3.5-turbo";
 
