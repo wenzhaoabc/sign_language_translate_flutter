7f484ebbc0f64fb46a9d711f46df1c6c9c71a705
wenzhaoabc <wenzhaoabc@gmail.com>
2023-07-28 18:24:08 +0800

    delete secret key

--- lib/toolkit/chat/provider/constants.dart
+++ lib/toolkit/chat/provider/constants.dart
@@ -1,6 +1,6 @@
 const baseUrl = 'http://43.153.22.13:8080/';
 // const baseUrl = 'https://api.openai.com/v1';
-const aiApiKey = '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';
+const aiApiKey = 'api_key';
 const aiModel = "gpt-3.5-turbo";
 
 abstract class StoreKey {
